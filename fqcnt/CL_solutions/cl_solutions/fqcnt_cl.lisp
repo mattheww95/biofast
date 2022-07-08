1b9131d633e324dd839a37b59d838bc9c209a2bd
Matthew Wells <mattwells9@shaw.ca>
2022-07-07 22:38:49 -0500

    setup asdf file, not working though...

--- fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
+++ fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
@@ -1,9 +1,10 @@
 ;;;;  A common lisp solution for counting symbols in a fastq file can be gzipped
 
 
+;;(load "~/quicklisp/setup.lisp")
+(ql:quickload "asdf")
 
-(ql:quickload :cl-ppcre)
-
+;;(require "cl-ppcre")
 (defun cmd-line-args ()
     "Read in the command line arguments and open with correct file handler"
     (setf file-arg (car *args*))
