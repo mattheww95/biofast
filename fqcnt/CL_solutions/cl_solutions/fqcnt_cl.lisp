427f11e33e4b37cbb5ae6d3b2f31bf0e2aa44d8b
Matthew Wells <mattwells9@shaw.ca>
2022-07-08 20:38:06 -0500

    initialized scripting format

--- fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
+++ fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
@@ -1,18 +1,20 @@
-;;;;  A common lisp solution for counting symbols in a fastq file can be gzipped
+#! /usr/bin/sbcl --script
+(load "/home/mattheww/.sbclrc") 
 
 
 ;;(load "~/quicklisp/setup.lisp")
-(ql:quickload "asdf")
+;;(ql:quickload "asdf")
 
+(ql:quickload "cl-ppcre")
 ;;(require "cl-ppcre")
-(defun cmd-line-args ()
+(defun cmd-line-args (args)
     "Read in the command line arguments and open with correct file handler"
-    (setf file-arg (car *args*))
+    (let ((file-arg (car args)))
     (if (not file-arg) 
     (progn (format t "~&~S~&" "No file input") (exit))
     (cl-ppcre:split "." file-arg)
     )
-    (eval file-arg)
+    (eval file-arg))
 )
 
-(format t "~&~S" (reverse (setf out (cmd-line-args))))
+(format t "~&~S~&" (reverse (setf out (cmd-line-args sb-ext:*posix-argv*))))
