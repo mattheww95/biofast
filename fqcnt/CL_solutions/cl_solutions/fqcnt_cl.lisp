2b55ebaa4d85a4f9545a5360c0d9a97eac21b1e8
Matthew Wells <mattwells9@shaw.ca>
2022-07-09 12:57:25 -0500

    updated lisp file reader

--- fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
+++ fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
@@ -5,11 +5,27 @@
 ;;(load "~/quicklisp/setup.lisp")
 ;;(ql:quickload "asdf")
 
-(ql:quickload "cl-ppcre")
-;;(require "cl-ppcre")
+;;(ql:quickload "cl-ppcre")
+
+(require "cl-ppcre")
+
+;; function to open plain text file and read it line by line
+(defun plain-text-fq (fp)
+    "Open a plain text file and read it line by line"
+    (let ((in (open fp :if-does-not-exist :error)))
+        (when in
+            (loop for line = (read-line in nil)
+                while line do (format t "~a~%" line))
+                (close in)
+        ))
+)
+
+
+
+
 (defun cmd-line-args (args)
     "Read in the command line arguments and open with correct file handler"
-    (let ((file-arg (car args)))
+    (let ((file-arg (cdr args)))
     (if (not file-arg) 
     (progn (format t "~&~S~&" "No file input") (exit))
     (cl-ppcre:split "." file-arg)
