c0925c37b6cb0aef05366babaa188823482dcf68
Matthew Wells <mattwells9@shaw.ca>
2022-07-12 20:29:02 -0500

    added global vars lisp solution

--- fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
+++ fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
@@ -1,4 +1,7 @@
 #! /usr/bin/sbcl --script
+
+;;; Count number of records in a fastq file, total length of sequences and quality
+;;; FQ is a typical 4 line fastq
 (load "/home/mattheww/.sbclrc") 
 
 
@@ -10,12 +13,23 @@
 (require "cl-ppcre")
 
 ;; function to open plain text file and read it line by line
+
+(defvar *seq-count* 0)
+(defvar *line-length* 0)
+(defvar *quality-length* 0)
+
 (defun plain-text-fq (fp)
     "Open a plain text file and read it line by line"
     (let ((in (open fp)))
         (when in
             (loop for line = (read-line in)
-                while line do (format t "~a~%" line))
+                while line do (
+                    (if (char= (char line 0) #\@)
+                        (progn ((let* ((sequence (read-line in)) (plus+ (read-line in)) (quality (read-line in)))
+                                    (format t "~&~S~&" sequence))))
+                        (continue))
+                    )
+                )
         (close in)))
 )
 
@@ -39,4 +53,5 @@
 )
 
 ;;(format t "~&~S~&" (reverse (setf out (cmd-line-args sb-ext:*posix-argv*))))
+
 (plain-text-fq "../../../biofast-data-v1/M_abscessus_HiSeq.fq")
