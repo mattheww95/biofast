74b099d8e6ecb989dbd6d645844f1d3ed769a24a
Matthew Wells <mattwells9@shaw.ca>
2022-07-16 17:13:28 -0500

    lisp soluition working now

--- fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
+++ fqcnt/CL_solutions/cl_solutions/fqcnt_cl.lisp
@@ -17,28 +17,57 @@
 (defvar *seq-count* 0)
 (defvar *line-length* 0)
 (defvar *quality-length* 0)
+(defvar *toss-away* 0)
+
+
+;; (defun plain-text-fq (fp)
+;;     "Open a plain text file and read it line by line"
+;;     (let ((in (open fp)))
+;;         (when in
+;;             (loop for line = (read-line in)
+;;                 while line do (
+;;                     (if (char= (char line 0) #\@)
+;;                         (progn ((let* ((sequence (read-line in)) (plus+ (read-line in)) (quality (read-line in)))
+;;                                     (form
+;;                                     at t "~&~S~&" sequence))))
+;;                       (continue))
+;;                    )
+;;                )
+;;        (close in)))
+;;)
+
+
+
+;;(defun plain-text-fq-2 (fp )
+  ;; "An alternative fastq text line reader"
+  ;; (let ((in (open fp)))
+  ;;   (when in
+  ;;     (loop for line = (read-line in)
+  ;;           while line do (
+  ;;                          (if (char= (char line 0) #\@)
+  ;;                              ((let* ((sequence (read-line in nil)) (plus+  (read-line in nil)) (quality = (read-line in nil)))
+  ;;                                        (format t "~S~&~S~&~S~&" sequence plus+ quality))))
+  ;;                          )))))
+
 
 (defun plain-text-fq (fp)
-    "Open a plain text file and read it line by line"
-    (let ((in (open fp)))
-        (when in
-            (loop for line = (read-line in)
-                while line do (
-                    (if (char= (char line 0) #\@)
-                        (progn ((let* ((sequence (read-line in)) (plus+ (read-line in)) (quality (read-line in)))
-                                    (format t "~&~S~&" sequence))))
-                        (continue))
-                    )
-                )
-        (close in)))
-)
+  "Test on file reading"
+  (let ((in (open fp :if-does-not-exist nil)))
+    (when in
+    (loop for line = (read-line in nil)
+          while line do (if (char= (char line 0) #\@)
+                            ;; can read in more line and add too global vars
+                             (progn
+                              (setf *line-length* (+ *line-length* (length (read-line in nil))))
+                              (read-line in nil)
+                              (setf *quality-length* (+ *quality-length* (length (read-line in nil))))
+                                     ;;(length (read-line in nil))
+                              )
+                             )
+                            )
+          )
+    (close in)))
 
-(defun plain-text-fq-2 (fp)
-    "An alternative way of reading a file in lisp"
-    (with-open-file (stream fp)
-        (loop for line = (read-line stream nil)
-            while line
-            collect line)))
 
 
 
